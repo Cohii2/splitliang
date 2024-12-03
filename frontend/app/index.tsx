9a82717d3656f117c0527a758cea706ab6ebd2d2
Cohii <hengjunxiangg@gmail.com>
2024-12-03 15:49:10 +0800

    Add react native paper
    Install react native paper for app UI.
    Add test button to `app/index` to test Button component from library.

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,15 +1,23 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import {Button} from "react-native-paper";
 
 export default function Index() {
-  return (
-    <View
-      style={{
+    return (
+        <View
+            style={styles.screenContainer}
+        >
+            <Text>Edit app/index.tsx to edit this screen.</Text>
+            <Button icon="account" mode="contained" onPress={() => console.log("Pressed test button")}>
+                Test Button
+            </Button>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    screenContainer: {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
-}
+    },
+});
