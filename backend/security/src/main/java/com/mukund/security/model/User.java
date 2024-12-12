23367a2619526ec2231e81080d676799cf087812
Ayagari Mukund <ayagarimukund@Ayagaris-MacBook-Air.local>
2024-12-12 15:21:52 +0800

    backend initial commit (for reals)

---
+++ backend/security/src/main/java/com/mukund/security/model/User.java
@@ -0,0 +1,44 @@
+package com.mukund.security.model;
+
+import jakarta.persistence.Id;
+
+public class User {
+
+    @Id
+    private int id;
+    private String username;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ backend/security/src/main/java/com/mukund/security/repo/UserRepo.java
@@ -0,0 +1,7 @@
+package com.mukund.security.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+
+}
